27b6ad3dcb63dc9742098f30864a67bb3bcab522
JaeLong03 <jaehwan4562@gmail.com>
2025-04-09 23:43:49 +0900

    4.9 update

---
+++ DS/DS031/main.cpp
@@ -0,0 +1,31 @@
+#include "stackInt.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size; 
+    string input; 
+    cin >>size; 
+
+    MyStackInt a(size);
+    cin.ignore(); 
+
+    while(true){ 
+        getline(cin, input); 
+        
+        if (input.substr(0,4) == "push") {
+            a.push(stoi(input.substr(5)));
+        } else if (input == "pop") {
+            a.pop();
+        } else if(input == "print") {
+            if(a.isEmpty())
+                cout << "Stack is empty"; 
+            else
+                a.display(); 
+            break;
+        }
+    }
+
+
+    return 0; 
+} 
---
+++ DS/DS031/stackInt.cpp
@@ -0,0 +1,62 @@
+#include "stackInt.h"
+#include <iostream>
+using namespace std;
+
+MyStackInt::MyStackInt(int size) { 
+    S = new int[size]; // creating the size of the Stack in memory 
+    this->size = size; 
+    top = -1; // intially set top of the Stack as -1;
+}
+
+MyStackInt::~MyStackInt() {
+    delete[] S; // free the memory 
+}
+
+void MyStackInt::push(int x) {
+    if (isFull()) { 
+        cout << "Stack Overflow!" << endl; 
+    }
+    else {  
+        top++; // increment top 
+        S[top] = x; // and push the element to Stack 
+    }
+}
+
+int* MyStackInt::pop() { 
+    int *x = S; 
+    if (isEmpty()) { 
+        cout << "Stack Underflow!" << endl; 
+    }
+    else {
+        x = &S[top]; // take out the element 
+        top--; // and decreament the size of the Stack 
+    }
+    return x;
+}
+
+bool MyStackInt::isFull() { 
+    if (top == size - 1) { 
+        return true;
+    }
+    return false;
+}
+
+bool MyStackInt::isEmpty() {
+    if (top == -1) {
+        return true;
+    }
+    return false;
+}
+
+void MyStackInt::display() { 
+    for (int i = top; i >= 0; i--) {
+        cout << S[i] << endl;
+    }
+}
+
+int MyStackInt::stackTop() {
+    if (isEmpty()) {
+        return -1;
+    }
+    return S[top];
+}
